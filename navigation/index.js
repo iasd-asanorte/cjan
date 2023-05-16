c079ee9582e7813f87310438f57ab906bca203a6
Wesin Ribeiro Alves <wesinec@gmail.com>
2023-05-16 16:35:41 -0300

    add start screen skeleton

--- navigation/index.js
+++ navigation/index.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import LoginScreen from "../screens/LoginScreen"
 import SignupScreen from "../screens/SignupScreen"
 import ChoiceScreen from "../screens/ChoiceScreen"
+import StartScreen from "../screens/StartScreen"
 
 const Root = createStackNavigator()
 
@@ -15,6 +16,7 @@ const Navigation = () => {
                 <Root.Screen name="Signup" component={SignupScreen} />
                 <Root.Screen name="Choice" component={ChoiceScreen} />
                 <Root.Screen name="Login" component={LoginScreen} />
+                <Root.Screen name="Start" component={StartScreen} />
             </Root.Navigator>
         </NavigationContainer>
     )
---
+++ screens/StartScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const StartScreen = () => {
+  return (
+    <Text>StartScreen</Text>
+  )
+}
+
+export default StartScreen
