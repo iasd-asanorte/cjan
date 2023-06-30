6afff8ebe0dc742c41b1ea2374eee82731d2a39c
wesin ribeiro <wesinec@gmail.com>
2023-06-29 23:10:29 -0300

    add user service

---
+++ services/UserService.js
@@ -0,0 +1,64 @@
+import Database from './firebase';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+
+export const login = async (email, password) => {
+    try{
+        // Logging in with email and password
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        // Getting the logged-in user
+        const user = userCredential.user;
+
+        // Returning the logged-in user or any other required information
+        return user;
+    } catch (error) {
+        console.error("Error loggin in: ", error);
+        throw error;
+    }
+    
+}
+
+export const logout = async () => {
+    try {          
+      await auth.signOut();
+      console.log("Logout successful!");
+      // Perform any desired actions after successful logout
+    } catch (error) {
+      console.error("Logout failed:", error);
+      // Handle the error as needed
+    }
+};
+
+export const recoverPassword = async (email) => {
+    try {
+      await auth.sendPasswordResetEmail(email);
+      console.log("Password recovery email sent successfully!");
+      // Perform any desired actions after the recovery email is sent
+    } catch (error) {
+      console.error("Password recovery failed:", error);
+      // Handle the error as needed
+    }
+};
+
+export const signUp = async (email, password, name) => {
+    try {      
+      const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+      const user = userCredential.user;
+  
+      // Save the user's name in a user collection
+      const db = Database;
+      const userRef = db.collection("users").doc(user.uid);
+  
+      await userRef.set({ name });
+  
+      console.log("Signup successful!");
+      console.log("User:", user);
+      // Perform any desired actions after successful signup
+    } catch (error) {
+      console.error("Signup failed:", error);
+      // Handle the error as needed
+    }
+};
+
