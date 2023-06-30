22936780d4b0c4461cff681242075f844c11de72
wesin ribeiro <wesinec@gmail.com>
2023-06-30 16:48:44 -0300

    add home screen skeleton

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import { Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const HomeScreen = ({navigation}) => {
+
+  useEffect(() => {    
+    
+  }, [])
+
+  return (
+    <Text>HomeScreen</Text>
+  )
+}
+
+export default HomeScreen
--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const StartScreen = () => {
+const StartScreen = ({navigation}) => {
+
+  useEffect(() => {    
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+
+      if(token !== null){
+          // validate token
+          navigation.navigate('Home');
+      }else{
+          navigation.navigate('Login');          
+      }
+    }
+    checkToken()
+  }, [])
+
   return (
     <Text>StartScreen</Text>
   )
