54633cab159656d27775f302b98b26985e7ab0be
wesin ribeiro <wesinec@gmail.com>
2023-06-30 16:49:25 -0300

    udpate navigation

--- navigation/index.js
+++ navigation/index.js
@@ -6,6 +6,7 @@ import LoginScreen from "../screens/LoginScreen"
 import SignupScreen from "../screens/SignupScreen"
 import ChoiceScreen from "../screens/ChoiceScreen"
 import StartScreen from "../screens/StartScreen"
+import HomeScreen from "../screens/HomeScreen"
 
 const Root = createStackNavigator()
 
@@ -13,10 +14,11 @@ const Navigation = () => {
     return(
         <NavigationContainer>
             <Root.Navigator screenOptions={{headerShown: false}}>
+                <Root.Screen name="Start" component={StartScreen} />
+                <Root.Screen name="Login" component={LoginScreen} />
                 <Root.Screen name="Signup" component={SignupScreen} />
                 <Root.Screen name="Choice" component={ChoiceScreen} />
-                <Root.Screen name="Login" component={LoginScreen} />
-                <Root.Screen name="Start" component={StartScreen} />
+                <Root.Screen name="Home" component={HomeScreen} />
             </Root.Navigator>
         </NavigationContainer>
     )
