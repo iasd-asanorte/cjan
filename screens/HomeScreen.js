1260ab22458aed0507def564b87717896d71f507
wesin ribeiro <wesinec@gmail.com>
2023-07-07 06:58:51 -0300

    update home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,11 +4,16 @@ import {
   TouchableOpacity,
   View,
   Image,
-  StyleSheet,
-
 } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Schedule } from '../components/Schedule';
+import Schedule from '../components/Schedule';
+import styles from '../theme/homeStyles';
+
+const event = {
+  title: 'Cantata de Natal',
+  local: 'Iasd Asa Norte',
+  date: '25/12/2020',
+}
 
 const HomeScreen = ({navigation}) => {
 
@@ -17,10 +22,12 @@ const HomeScreen = ({navigation}) => {
   }, [])
 
   return (
-    <View style={styles.screen}>
-      <Schedule />
+    <View style={styles.container}>
+      <Text style={styles.title}>Destaque</Text>
+      <Schedule title={event.title} local={event.local} date={event.date} />   
+      <Text style={styles.title}>Atividade Recente</Text>
     </View>
-  )
+  );
 }
 
 export default HomeScreen
