73d36462df881a02a93fbd4c8b98a65caaa4a32a
wesin ribeiro <wesinec@gmail.com>
2023-07-14 12:28:01 -0300

    update global styles

--- theme/globalStyles.js
+++ theme/globalStyles.js
@@ -43,7 +43,33 @@ const styles = StyleSheet.create({
     status:{
         fontSize: 12,
         color: colors.neutro,        
-    }
+    },
+    textInput:{                
+        borderRadius: 10,
+        height: 80,
+        width: '100%',
+        padding: 10,
+        borderColor: '#ffffff',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        marginBottom: 15,
+        color: '#ffffff'        
+    },
+    button: {
+        width: '100%',
+        height: 80,
+        backgroundColor: '#5f3ab8',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',        
+
+    },
+    textButton: {
+        fontSize: 22,
+        color: '#ffffff',
+        fontStyle: 'normal',
+        fontWeight: 700,
+    },
 });
 
 export default styles;
