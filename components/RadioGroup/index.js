6641b559bfb491a5e88f1e7ce0cad2921566885b
wesin ribeiro <wesinec@gmail.com>
2023-07-27 09:38:49 -0300

    update radio group

--- components/RadioGroup/index.js
+++ components/RadioGroup/index.js
@@ -3,16 +3,22 @@ import { View, TouchableOpacity, Text } from 'react-native'
 import RaddioButton from '../RadioButton'
 import styles from './styles'
 
-const RadioGroup = ({data}) => {
+const RadioGroup = ({data, onSelect}) => {
     const [userOption, setUserOption] = useState();
+    const isSelected = (value) => {
+        return value === userOption;
+    }
+    const handleSelect = (value) => {
+        setUserOption(value);
+        onSelect(value);
+    }
     return (
         <View style={styles.container}>
             {data.map((item) => (
-                <TouchableOpacity onPress={() => setUserOption(item.value)} key={item.value}>
-                    <RaddioButton selected={item.selected} value={item.value} />
+                <TouchableOpacity onPress={() => handleSelect(item.value)} key={item.id}>
+                    <RaddioButton selected={isSelected(item.value)} value={item.value} />
                 </TouchableOpacity>
-            ))}
-        <Text> User option: {userOption}</Text>
+            ))}        
         </View>
     )
 }
