1b5f9aecef7a41bba1703f63103f55817e97d5ae
wesin ribeiro <wesinec@gmail.com>
2023-07-27 09:39:04 -0300

    adjust color of the component

--- components/RadioButton/index.js
+++ components/RadioButton/index.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { View, Text } from 'react-native'
 import styles from './styles'
 import { Ionicons } from '@expo/vector-icons'
+import colors from '../../theme/colors'
 
 const RaddioButton = ({selected, value }) => {            
+
+  const getColor = (selected) => {
+    return selected ? colors.white : colors.neutro;
+  }
 
   return (
     // create a raddion button component
     <View style={styles.container}>
         <View style={styles.radioCircle}>
             {selected 
-                ? <Ionicons name="radio-button-on" size={24} color="white" /> 
-                : <Ionicons name="radio-button-off" size={24} color="white" /> 
+                ? <Ionicons name="radio-button-on" size={24} color="violet" /> 
+                : <Ionicons name="radio-button-off" size={24} color="grey" /> 
             }
         </View>
-        <Text style={styles.radioText}>{value}</Text>
+        <Text style={[styles.radioText, {color: `${getColor(selected)}`} ]}>{value}</Text>
     </View>
 
   )
--- components/RadioButton/styles.js
+++ components/RadioButton/styles.js
@@ -4,11 +4,10 @@ import colors from "../../theme/colors";
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        width: '100%',
-        height: 60,                
+        width: '100%',                        
         alignItems: 'center',
         paddingHorizontal: 10,
-        marginVertical: 5,
+        marginVertical: 5,        
     },
     radioCircle: {
         height: 30,
@@ -18,8 +17,7 @@ const styles = StyleSheet.create({
     },
     radioText: {
         marginLeft: 10,
-        fontSize: 14,
-        color: colors.white,
+        fontSize: 14,        
     },
 });
 
